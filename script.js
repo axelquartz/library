8ce7f74e0af8099790082305897e01c26189d417
Axel <axelarep@gmail.com>
2022-03-27 23:49:36 -0600

    Basic functionality and viusal plcaeholders have been added

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let myLibrary = [];
 
 function Book(title){
-    
         this.title = title;
 }
 
@@ -14,10 +13,28 @@ function Book(title){
 
 function displayBooks(title){
     title = prompt('Write a Book Name')
-    myLibrary.push(new Book(title)) ;
-    for (let i = 0; i < myLibrary.length; i++) {
-        console.log(myLibrary[i].title);
+    myLibrary.push(new Book(title));
+    for (let i = myLibrary.length - 1; i != myLibrary.length; i++) {
+        cardsContainer = document.querySelector('.cards-container')
+        let newCard = document.createElement('div')
+        newCard.className = 'card'
+        cardsContainer.append(newCard)
+
+        
+        newP = document.createElement('p')
+        newCard.append(newP)
+        newP.append(myLibrary[i].title);
+        console.log(i);
+        // console.log(myLibrary[i].title);
+        
+        
+        
+         
         
     }
 }
+
+let testButton = document.querySelector('#test-button');
+testButton.addEventListener('click', displayBooks)
+
 
