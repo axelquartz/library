4d3e3142e3ae24ede0f65c04a95667c81cb82bed
Axel <axelarep@gmail.com>
2022-03-28 23:39:25 -0600

    Core functionality is working, script is at 60% and style is not defined yet

--- script.js
+++ script.js
@@ -1,40 +1,58 @@
 let myLibrary = [];
 
-function Book(title){
+function Book(title, author){
         this.title = title;
+        this.author = author;
 }
 
-// function passBook(title){
-//    myLibrary.push(new Book(title)) ;
+function displayBooks(title, author){
+    title = document.getElementById('name').value
+    author = document.getElementById('author').value;
+    // let grade = document.getElementById('grade').value;
 
-//     // myLibrary.push(title)
-//     console.log(myLibrary[i].title);
-// }
+    myLibrary.push(new Book(title, author));
+    
 
-function displayBooks(title){
-    title = prompt('Write a Book Name')
-    myLibrary.push(new Book(title));
     for (let i = myLibrary.length - 1; i != myLibrary.length; i++) {
         cardsContainer = document.querySelector('.cards-container')
         let newCard = document.createElement('div')
-        newCard.className = 'card'
-        cardsContainer.append(newCard)
-
-        
+        newCard.className = 'card';
+        newCard.setAttribute('id', i);
+        cardsContainer.append(newCard)    
+        newRemoveButton = document.createElement('button')    
         newP = document.createElement('p')
+        newCard.append(newRemoveButton)
+
+        newRemoveButton.addEventListener('click', function test(){
+            alert(myLibrary[i].title)
+        })
+
         newCard.append(newP)
         newP.append(myLibrary[i].title);
+        newBr = document.createElement('br')
+        newP.append(newBr)
+        newP.append(myLibrary[i].author)
+
         console.log(i);
         // console.log(myLibrary[i].title);
+        document.getElementById('name').value = '';
+        document.getElementById('author').value = '';
+
         
-        
-        
-         
         
     }
 }
 
-let testButton = document.querySelector('#test-button');
-testButton.addEventListener('click', displayBooks)
+let entry = document.getElementById('entry');
+entry.addEventListener('click', displayBooks);
 
+// let row = 1;
 
+// function displayDetails(){
+    
+//     let name = document.getElementById('name').value;
+//     let age = document.getElementById('age').value;
+//     let grade = document.getElementById('grade').value;
+
+//     alert(name + age + grade)
+// }
